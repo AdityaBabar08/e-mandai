9327b2be60b31e214167bcf871a8e6bad401a8f5
Aditya Babar <babaraditya02@gmail.com>
2026-06-15 12:08:26 +0530

    Initail Commit

---
+++ src/main/java/com/adityababar/e_mandai/EMandaiApplication.java
@@ -0,0 +1,13 @@
+package com.adityababar.e_mandai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMandaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EMandaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adityababar/e_mandai/EMandaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adityababar.e_mandai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMandaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
